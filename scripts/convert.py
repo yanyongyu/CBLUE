40e894e6ccdcd1c7237cf6699ad9cda359b3a6f0
Ju4tCode <42488585+yanyongyu@users.noreply.github.com>
2023-05-25 14:16:21 +0800

    :sparkles: add few shot data switch

--- scripts/convert.py
+++ scripts/convert.py
@@ -11,18 +11,20 @@ def convert(result_dir: str):
 
     # convert CMeEE v1 to v2
     file = dir / "CMeEE_test.json"
-    data = json.loads(file.read_text())
+    data = json.loads(file.read_text(encoding="utf-8"))
     for sample in data:
         for entity in sample["entities"]:
             entity["end_idx"] = entity["end_idx"] + 1
 
     new_file = dir / "CMeEE-V2_test.json"
-    new_file.write_text(json.dumps(data, indent=2, ensure_ascii=False))
+    new_file.write_text(
+        json.dumps(data, indent=2, ensure_ascii=False), encoding="utf-8"
+    )
 
     # convert CMeIE v1 to v2
     file = dir / "CMeIE_test.jsonl"
     new_file = dir / "CMeIE-V2_test.jsonl"
-    new_file.write_text(file.read_text())
+    new_file.write_text(file.read_text(encoding="utf-8"), encoding="utf-8")
 
 
 if __name__ == "__main__":
---
+++ scripts/data_switch.py
@@ -0,0 +1,79 @@
+import sys
+import json
+import random
+import shutil
+from pathlib import Path
+from typing import Any, List
+
+PROJECT_ROOT = Path(__file__).parent.parent
+DATASET_DIR = PROJECT_ROOT / "CBLUEDatasets"
+
+ORIGIN_DATASET_DIR = PROJECT_ROOT / "CBLUEDatasets_origin"
+FEW_SHOT_DATASET_DIR = PROJECT_ROOT / "CBLUEDatasets_few_shot"
+
+FEW_SHOT_SAMPLES = 100
+
+
+def init():
+    if ORIGIN_DATASET_DIR.exists() or FEW_SHOT_DATASET_DIR.exists():
+        print("Dataset already exists. Skip init.")
+        return
+
+    # generate few-shot dataset
+    print("Generating few-shot dataset...")
+    generate_dataset()
+
+
+def generate_dataset():
+    FEW_SHOT_DATASET_DIR.mkdir(parents=True, exist_ok=True)
+
+    for dataset in (
+        "CMeEE",
+        "CHIP-CTC",
+        "CHIP-CDN",
+        "CHIP-STS",
+        "KUAKE-QIC",
+        "KUAKE-QQR",
+        "KUAKE-QTR",
+    ):
+        origin_dataset_dir = DATASET_DIR / dataset
+        target_dataset_dir = FEW_SHOT_DATASET_DIR / dataset
+        shutil.copytree(origin_dataset_dir, target_dataset_dir)
+
+        train_file = target_dataset_dir / f"{dataset}_train.json"
+        data: List[Any] = json.loads(train_file.read_text(encoding="utf-8"))
+        few_shot_data = random.sample(data, FEW_SHOT_SAMPLES)
+        train_file.write_text(
+            json.dumps(few_shot_data, ensure_ascii=False, indent=2),
+            encoding="utf-8",
+        )
+
+    for dataset in ("CMeIE",):
+        origin_dataset_dir = DATASET_DIR / dataset
+        target_dataset_dir = FEW_SHOT_DATASET_DIR / dataset
+        shutil.copytree(origin_dataset_dir, target_dataset_dir)
+
+        train_file = target_dataset_dir / f"{dataset}_train.jsonl"
+        data = train_file.read_text(encoding="utf-8").splitlines()
+        few_shot_data = random.sample(data, FEW_SHOT_SAMPLES)
+        train_file.write_text("\n".join(few_shot_data), encoding="utf-8")
+
+
+def switch_dataset():
+    if ORIGIN_DATASET_DIR.exists() and not FEW_SHOT_DATASET_DIR.exists():
+        DATASET_DIR.rename(FEW_SHOT_DATASET_DIR)
+        ORIGIN_DATASET_DIR.rename(DATASET_DIR)
+    elif FEW_SHOT_DATASET_DIR.exists() and not ORIGIN_DATASET_DIR.exists():
+        DATASET_DIR.rename(ORIGIN_DATASET_DIR)
+        FEW_SHOT_DATASET_DIR.rename(DATASET_DIR)
+    else:
+        raise RuntimeError("Dataset not valid!")
+
+
+def main():
+    init()
+    switch_dataset()
+
+
+if __name__ == "__main__":
+    main()
